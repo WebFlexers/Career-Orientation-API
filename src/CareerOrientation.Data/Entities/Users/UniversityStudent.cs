589eb629347f23ec421c4c0cf5398f1fc3297ceb
StyleM <57811193+mStylias@users.noreply.github.com>
2023-06-09 17:26:26 +0300

    Add Users

---
+++ src/CareerOrientation.Data/Entities/Users/UniversityStudent.cs
@@ -0,0 +1,13 @@
+﻿using CareerOrientation.Data.Entities.Specialties;
+
+namespace CareerOrientation.Data.Entities.Users;
+
+public class UniversityStudent
+{
+    public string UserId { get; set; }
+    public int Semester { get; set; }
+
+    public int? TrackId { get; set; }
+    public User User {  get;set; }
+    public Track? Track { get; set; }
+}
---
+++ src/CareerOrientation.Data/Entities/Users/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CareerOrientation.Data.Entities.Users;
+
+public class User : IdentityUser
+{
+    public bool IsProspectiveStudent { get; set; }
+
+    public UniversityStudent? UniversityStudent { get; set; }
+}
